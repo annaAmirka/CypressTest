88d0625473ce43af9b913b0dbbfda86cb0f183eb
annaAmirka <anna.amirkhanyan83@gmail.com>
2023-04-09 16:55:06 +0400

    sign in test

--- cypress/e2e/T_1.cy.js
+++ cypress/e2e/T_1.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import { loginPage } from "../pages/Login.js";
+import { data } from "../utils/data";
+describe("SignIn", () => {
+  it("Verify login functionality with valid credentials", () => {
+   loginPage.visit();
+   loginPage.getUsername(data.username)
+   loginPage.getPassword('username')
+   loginPage.submit();
+   cy.url().should('include', '/parabank/overview.htm')
+  });
+});
